a6148dd43be3b86a885b9fbd870ee0b841bb3be7
navyaacharya05 <navyaacharya05@gmail.com>
2023-03-04 13:39:50 +0530

    angular started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-firstapp';
+  firstName="Navya";
+  lastName="Acharya";
+
+  submit(){
+    console.log(this.firstName,this.lastName);
+  }
 }
